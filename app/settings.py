a812e942a98d68b47c1eef075a648108e9b04284
vachagancode <140316187+vachagancode@users.noreply.github.com>
2023-10-07 16:04:01 +0400

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECRET_KEY = "29f393d5447c39b27cab4ea18b996be4"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get(DEBUG, "False").split(" ")
+DEBUG = os.environ.get(DEBUG, "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 # ALLOWED_HOSTS = []
